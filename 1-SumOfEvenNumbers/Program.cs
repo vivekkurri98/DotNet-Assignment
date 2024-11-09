1e861f14faf13010fbe1f985407f4ac85bb0bbc7
vivekkurri98 <73775052+vivekkurri98@users.noreply.github.com>
2024-11-09 20:19:06 +0530

    Sum of Even Numbers

---
+++ 1-SumOfEvenNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+Console.WriteLine("1) Given an array of integers, write a C# method to total all the values that are even numbers.");
+
+int[] array = { 1, 2, 3, 4, 5, 6 };
+int result = 0;
+foreach (int i in array)
+{
+    if (i % 2 == 0)
+    {
+        result += i;
+    }
+}
+Console.WriteLine($"Sum of Even Number is {result}");
+Console.ReadLine();
